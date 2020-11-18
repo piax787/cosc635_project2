d9aa49a96b1c2601b62afafa1eb5e644364cc886
piax787 <ammarfqureshi@gmail.com>
2020-11-17 22:45:05 -0600

    init javafx project

---
+++ src/main/java/com/cosc635/project2_gui/SystemInfo.java
@@ -0,0 +1,13 @@
+package com.cosc635.project2_gui;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.cosc635.project2_gui {
+    requires javafx.controls;
+    exports com.cosc635.project2_gui;
+}
